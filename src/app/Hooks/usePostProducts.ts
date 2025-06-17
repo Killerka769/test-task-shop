6eb276997b08cb0c193100c01166ae567c97018d
Killerka769 <gaydar.123@mail.ru>
2025-06-17 21:11:15 +0300

    Реализован proxy для заказов, продуктов и отзывов через API routes

--- src/app/Hooks/usePostProducts.ts
+++ src/app/Hooks/usePostProducts.ts
@@ -1,17 +1,20 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import axios from "axios"
 import { TPostProduct } from "../Types/Types";
 
 
 const usePostProducts = () => {
+    const queryClient = useQueryClient();
     const { mutate, isPending, isSuccess, isError } = useMutation({
         mutationFn: (el : TPostProduct)=>{
-            return axios.post('http://o-complex.com:1337/order', el);
+            return axios.post('/api/order', el);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
         }
     })
-    // onSuccess: () => {
-    //     queryClient.invalidateQueries(['products']);
-    // }
+
+    
   return { mutatePostProduct : mutate, isPendingPostProduct : isPending, isSuccessPostProduct : isSuccess, 
    isErrorPostProduct : isError }
 }
